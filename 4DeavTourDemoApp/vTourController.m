a96e1e19c1ff216445d3cfc452e661c4fb157d16
Rohan Jariwala <rohanj1607@gmail.com>
2017-03-17 19:20:30 +0530

    Base URL goes to default

--- 4DeavTourDemoApp/vTourController.m
+++ 4DeavTourDemoApp/vTourController.m
@@ -51,9 +51,7 @@
 -(void)setupvTourView{
     self.vtourView = [[vTourView alloc]initWithFrame:self.view.frame withDelegate:self];
     [self.view addSubview:self.vtourView];
-    [self.vtourView setBaseURL:demoConstants->imageBaseURL]; //Pass baseURL of cleartrip web server structure
-    [self.vtourView setJSONBaseURL:demoConstants->jsonBaseURL];
-    
+
     NSInteger hotelNumber = [[NSUserDefaults standardUserDefaults] integerForKey:@"Hotel_Number"];
     NSArray *hotels = [self.hotelsData objectForKey:@"Hotels"];
     NSDictionary *hotel = [hotels objectAtIndex:hotelNumber];
